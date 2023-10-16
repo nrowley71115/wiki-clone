ee590ed8a95ff6b85225da258bbabce8fe474433
Nathan Rowley <nrowley71115@gmail.com>
2023-10-16 06:21:25 -0500

    added index page links, and completed entire search feature. It works through the entire layout.html. If an exact match is typed in, then it goes to that page. Otherwise, it will pull a list of all entries that contain your search as a sub-string

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,7 @@
+from django import forms
 from django.shortcuts import render
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -10,8 +10,6 @@ def index(request):
 
 # open wiki entry
 def entry(request, title):
-
-    
     if title in util.list_entries():
         return render(request, "encyclopedia/entry.html", {
             "entry": util.get_entry(title),
@@ -19,5 +17,33 @@ def entry(request, title):
         })
     else:
         return render(request, "encyclopedia/error.html", {
-            "error": "Page not found"
+            "error": "404 Page not found"
         })
+    
+# search wiki entry
+def search(request):
+    if request.method == "POST":
+        search_str = request.POST.get('q')
+        # Check for exact match
+        if search_str in util.list_entries():
+            return render(request, "encyclopedia/entry.html", {
+                "entry": util.get_entry(search_str),
+                "title": search_str
+            })
+        # Check for sub-search_string matches
+        else:
+            sub_str = []
+            for entry in util.list_entries():
+                if search_str.lower() in entry.lower():
+                    sub_str.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "entries": sub_str,
+                "str_search": search_str,
+              })
+    # if request is not POST
+    else:
+        # redirect to index page
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+        
